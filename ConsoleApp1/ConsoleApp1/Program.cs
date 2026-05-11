e2927f48710670a26395c7516ae2739576e3461d
semediy <c.semedii.yuliia@student.uzhnu.edu.ua>
2026-05-11 12:47:44 +0300

    First commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int largeNumber = 300;
+byte smallNumber = (byte)largeNumber; // Переповнення, оскільки byte не може зберігати значення більше 255
+Console.WriteLine(smallNumber); // Виведе 44 (результат переповнення)
